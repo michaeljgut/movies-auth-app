da0e7eef0cf7f54a1dd12295b7c12317f7bbaa86
Michael Gutleber <michaeljgut@gmail.com>
2017-08-28 11:49:06 -0400

    Add movies show page

--- models/movie.js
+++ models/movie.js
@@ -6,4 +6,8 @@ Movie.findAll = () => {
   return db.query('SELECT * FROM movies ORDER BY id ASC');
 };
 
+Movie.findById = id => {
+  return db.oneOrNone(`SELECT * FROM movies WHERE id = $1`, [id]);
+};
+
 module.exports = Movie;
